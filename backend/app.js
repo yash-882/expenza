b3faf39d13781669e6e9159b267295bb9b713842
yash <yashh2nd@gmail.com>
2025-05-27 03:43:08 +0530

    feat: create authentication Router with /sign-up and /login endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import authRouter from './routes/auth-route.js';
 import GlobalErrorHandler from './errors/global-err-handler.js';
 
 const app = express();
@@ -7,6 +7,8 @@ const app = express();
 // parse json data
 app.use(express.json())
 
+// authentication route(sign-up, login, change password, etc)
+app.use('/api/expenza/auth', authRouter);
 
 // global error handler
 app.use(GlobalErrorHandler)
