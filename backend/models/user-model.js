8b3f6246883c435ea84b99f2f5ae67ed0abcb329
yash <yashh2nd@gmail.com>
2025-05-26 14:28:18 +0530

    feat: add User Model for creating profile

---
+++ backend/models/user-model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true,
+    },
+
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        trim: true,
+        lowercase: true,
+        unique: true,
+        validate: {
+            validator: v => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v),
+            message: v => 'Invalid Email format!'
+        }
+    },
+    password: {
+        type: String,
+        required: [true, 'Create a password'],
+        minlength: [8, 'Password must contain atleast 8 characters']
+    },
+    confirmPassword: {
+        type: String,
+        required: [true, 'Confirm the Password']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+userSchema.pre('save', async function (next) {
+
+    if (this.password !== this.confirmPassword)
+        return next(new Error('Please re-enter your Password correctly'))
+
+    this.password = await bcrypt.hash(this.password, 12);
+    this.confirmPassword = undefined;
+
+    return next();
+})
+
+const userModel = mongoose.model('users', userSchema)
+
+export default userModel
