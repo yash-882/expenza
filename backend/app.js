ecae76a529b2140d5a7aa49f6d1868c0b745b80e
yash <yashh2nd@gmail.com>
2025-05-26 21:51:00 +0530

    feat: add GlobalErrorHandler for handling errors(focuses on develeopment errors for now)

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
 
+import GlobalErrorHandler from './errors/global-err-handler.js';
+
 const app = express();
 
 // parse json data
 app.use(express.json())
 
+
+// global error handler
+app.use(GlobalErrorHandler)
 
 export default app;
