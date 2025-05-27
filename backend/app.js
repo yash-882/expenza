c4702ecb512b9e33912e88c6a317cf57e0b309dc
yash <yashh2nd@gmail.com>
2025-05-28 04:53:46 +0530

    feat: Add middleware to secure sensitive endpoints and initialize Cookie parsing

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import authRouter from './routes/auth-route.js';
 import GlobalErrorHandler from './errors/global-err-handler.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 // parse json data
 app.use(express.json())
+
+// parse cookies
+app.use(cookieParser())
 
 // authentication route(sign-up, login, change password, etc)
 app.use('/api/expenza/auth', authRouter);
