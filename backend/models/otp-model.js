480bbabb30da1ad25e4cb40ef1164dc3be88b90f
yash <yashh2nd@gmail.com>
2025-05-30 04:28:12 +0530

    feat: define Schema for storing OTP temporarily

---
+++ backend/models/otp-model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const OTPSchema = mongoose.Schema({
+    otp: {
+        type: String,
+        unique: true
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 300, //OTP expires in 5 minutes 
+    }
+})
+
+const OTPModel = mongoose.model('otp', OTPSchema)
+
+export default OTPModel;
