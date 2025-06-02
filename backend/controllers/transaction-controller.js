0e1223f19128e3df11d17dd597ff9dce180275d6
yash <yashh2nd@gmail.com>
2025-06-03 05:19:49 +0530

    Use filtered query string in transaction-controller.js

--- backend/controllers/transaction-controller.js
+++ backend/controllers/transaction-controller.js
@@ -78,7 +78,10 @@ const getTransactionByID = wrapper(async(req, res, next) => {
 // get transactions
 const getTransactions = wrapper(async(req, res, next) => {
     const userID = req.user._id //user's ID
-    const transactions =  await transactionModel.find({user: userID})
+    const filter = req.filteredQuery; //sanitized query
+
+    const transactions =  await transactionModel.find(
+        {...filter.query, user: userID}).sort(filter.sortBy).select(filter.selectedFields)
 
     // no transactions were found
     if(transactions.length == 0){
@@ -172,9 +175,11 @@ const deleteTransactionByID = wrapper(async(req, res, next) => {
 // delete multiple transactions
 const deleteTransactions = wrapper(async(req, res, next) => {
     const userID = req.user._id; //user's ID
+    const filter = req.filteredQuery;
+    
     // delete all...
     const transaction =  await transactionModel.deleteMany({
-        ...req.query, 
+        ...filter.query, 
         user: userID})
 
     // no transactions were found
