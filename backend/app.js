8ca3e97d867f326c0ea3dcf3a777053f376b6e90
yash <yashh2nd@gmail.com>
2025-06-03 02:37:50 +0530

    Install 'qs' package to parse query strings

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import authRouter from './routes/auth-route.js';
 import GlobalErrorHandler from './errors/global-err-handler.js';
 import cookieParser from 'cookie-parser';
 import transactionRouter from './routes/transaction-route.js';
+import qs from 'qs';
 
 const app = express();
 
@@ -11,6 +12,12 @@ app.use(express.json())
 
 // parse cookies
 app.use(cookieParser())
+
+// tells express to use 'qs' package for parsing query strings instead of default parser
+  app.set('query parser', (queryString) => {
+    // 'queryString' is a raw string
+    return qs.parse(queryString)
+})
 
 // authentication route(sign-up, login, change password, etc)
 app.use('/api/expenza/auth', authRouter);
