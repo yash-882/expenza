e15780667943a6eb9448b310cdd90e134c06daf1
yash <yashh2nd@gmail.com>
2025-06-03 22:58:49 +0530

    refactor: update api prefix path to /api

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import authRouter from './routes/auth-route.js';
-import GlobalErrorHandler from './errors/global-err-handler.js';
-import cookieParser from 'cookie-parser';
-import transactionRouter from './routes/transaction-route.js';
-import qs from 'qs';
 
+// custom modules
+import GlobalErrorHandler from './errors/global-err-handler.js';
+
+// routers
+import authRouter from './routes/auth-route.js';
+import transactionRouter from './routes/transaction-route.js';
+import accountSettingRouter from './routes/account-setting.-route.js';
+
+// third-party packages
+import cookieParser from 'cookie-parser';
+import qs from 'qs';
+// app
 const app = express();
 
 // parse json data
@@ -20,8 +27,8 @@ app.use(cookieParser())
 })
 
 // authentication route(sign-up, login, change password, etc)
-app.use('/api/expenza/auth', authRouter);
-app.use('/api/expenza/transaction', transactionRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/transaction', transactionRouter);
 
 // global error handler
 app.use(GlobalErrorHandler)
