4ef91c7cf6a729c27b48a3155b8802c2fe4ea730
yash <yashh2nd@gmail.com>
2025-06-10 03:07:21 +0530

    INSTALL react-router-dom and bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,20 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    
-    </>
-  )
+  <div className="container">
+    <nav className="navbar">
+      <div className="logo">
+        <h2>Expense Tracker</h2>
+      </div>
+      <ul className="nav-links">
+        <li><a href="#dashboard">Dashboard</a></li>
+        <li><a href="#expenses">Expenses</a></li>
+        <li><a href="#income">Income</a></li>
+        <li><a href="#reports">Reports</a></li>
+      </ul>
+    </nav>
+  </div>
+)
 }
 
 export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Routes from './routes/routes.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {Routes()}
   </StrictMode>,
 )
