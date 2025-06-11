4d47e4ef88b5ee1610e8087178fd75d97b162db0
yash <yashh2nd@gmail.com>
2025-06-11 20:48:03 +0530

    feat: Add alias for folders: components, constants, assets, pages and utils in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+      '@constants': '/src/constants',
+      '@assets': '/src/assets',
+      '@pages': '/src/pages',
+      '@utils': '/src/utils'
+    }
+  }
 })
