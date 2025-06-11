4471e2cfb21dee1f7fbfcdccc0ec2fa36ac55ad1
yash <yashh2nd@gmail.com>
2025-06-11 20:28:12 +0530

    feat: Setup react-router-dom and bootstrap

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Router from './routes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* routes */}
+    {Router()} 
+
   </StrictMode>,
 )
