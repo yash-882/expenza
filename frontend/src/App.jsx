2db55079a56094fe605293bf3c18beb30984493b
yash <yashh2nd@gmail.com>
2025-06-13 00:39:36 +0530

    feat: Add navigation bar with styles and store links of it in an array

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    {/* Navigation bar */}
+    <Navbar/>
+    <Outlet/>
     </>
   )
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import {Link} from "react-router-dom"
+import { NavLinks } from '@constants/nav-bar/links.js';
+import ExpenzaLogo from "@assets/images/EXPENZA-LOGO.png"
+
+function Navbar() {
+
+let [activeTab, setActiveTab] = useState('/')
+
+
+
+  return (
+    // Navbar
+    <div className="container-fluid custom-navbar position-fixed py-1">
+        {/* Nav links */}
+            <div className="d-flex align-items-center justify-content-evenly" id="navbarContent">
+
+{/* Website logo */}
+        <div className='expenza-logo h-100'>
+              {/* Image logo/Link */}
+              <a href='http://localhost:5173' className='d-block'>
+            <img alt=""
+                height={'50px'} 
+                width='170px' 
+                src={ExpenzaLogo}
+                style={
+                  {objectFit: "cover"}}/>
+            </a>
+        
+        </div>
+
+        {/* list of nav links */}
+        <ul className="d-flex border-0 text-white py-2 gap-2 list-unstyled nav-tabs mb-2 mb-lg-0">
+
+            {
+              //Rendering links...
+                NavLinks.map(({label, icon: Icon, path, className}, index) => (
+                    <li className="nav-item" key={index}>
+
+                        {/* Link */}
+            <Link onClick={()=> setActiveTab(path)} 
+              className={`nav-link  nav-link-css border-0 rounded-4 p-2 ${
+                activeTab === path ? 'link-active' : ''
+              }`} to= {path}>
+                
+                {/* link label with icon */}
+                <Icon className={className} />
+                
+                {/* Link's label */}
+                 <span>{label}</span></Link>
+          </li>
+
+              ))
+          }
+
+        </ul>
+      </div>
+
+    </div>
+  );
+}
+
+export default Navbar
