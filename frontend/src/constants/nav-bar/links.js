3e622313ac21d00b972b9438a29467d8c94fa15a
yash <yashh2nd@gmail.com>
2025-06-13 00:39:36 +0530

    feat: Add navigation bar with styles and store links of it in an array

---
+++ frontend/src/constants/nav-bar/links.js
@@ -0,0 +1,24 @@
+// Navigation-bar links
+import {CreditCardIcon, PlusCircle, UserCircle2 } from "lucide-react"
+export const NavLinks =  [   {
+        // All transactions
+            label: 'Transactions',
+            path: '/',
+            icon: CreditCardIcon ,
+            className: 'nav-icon me-2'
+        },
+        // link to add transaction
+        {
+            label: 'Add Transaction',
+            path: '#add-transaction',
+            icon: PlusCircle,
+            className: 'nav-icon me-2 text-info'
+        },
+        // settings
+        {
+            label: 'Settings',
+            path: '#settings',
+            icon: UserCircle2,
+            className: 'nav-icon me-2'
+        },
+]
