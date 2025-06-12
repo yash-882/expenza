203443608d394033dfa782cd9593b95399e47876
yash <yashh2nd@gmail.com>
2025-06-13 02:59:31 +0530

    feat: Add main tag to contain the main content and set margin-top of it dynamically

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import { Outlet } from 'react-router-dom'
 
 function App() {
+  let [marginTop, setMarginTop] = useState('');
+  
+  function setNavbarRef(navbarRef){
+
+    let height = navbarRef?.current?.offsetHeight || 60; //assuming 60px height of navbar if not found
+      
+    //  set margin of main content(below the navbar)
+     setMarginTop(`${height}px`)
+  }
 
   return (
     <>
     {/* Navigation bar */}
-    <Navbar/>
+    <Navbar setNavbarRef = {setNavbarRef}/>
+
+    {/* Main content */}
+    <main style={{ marginTop: marginTop }}> 
+
     <Outlet/>
+
+    </main>
     </>
   )
 }
