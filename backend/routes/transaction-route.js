9965e36ca4c03f6a1bf5cf3510de21f47e8b7e75
yash <yashh2nd@gmail.com>
2025-06-14 01:52:17 +0530

    revert: Move protect(auth) middleware globally to app.js
    - Removes global 'protect(auth)' from app.js
    - Keeps 'resetBudget' in transaction-route.js

--- backend/routes/transaction-route.js
+++ backend/routes/transaction-route.js
@@ -1,6 +1,14 @@
 import { Router } from 'express'
+import authController from '../controllers/auth-controller.js'
 import transactionController from '../controllers/transaction-controller.js'
+import settingController from '../controllers/setting-controller.js'
 const transactionRouter = Router()
+
+// use middleware to protect routes
+transactionRouter.use(authController.protect)
+
+// handler to reset budget automatically after 1 month
+transactionRouter.use(settingController.resetBudget)
 
 // get all transactions
 transactionRouter.route('/')
