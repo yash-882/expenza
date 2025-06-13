0716390dc929e7ef84e22e0d5ec15d5d46cc798d
yash <yashh2nd@gmail.com>
2025-06-14 05:15:42 +0530

    feat: Add layout with styles to display all transactions

--- frontend/src/pages/Transactions.jsx
+++ frontend/src/pages/Transactions.jsx
@@ -8,9 +8,11 @@ function Transactions() {
         <div className="row d-flex justify-content-center">
             {/* options (filter, sort, etc) */}
             <div 
-            className="col-11 col-xl-8 customize-transactions-options rounded-2 py-2 border border-secondary ">
+            className="col-12 mb-3 col-xl-8 customize-transactions-options py-2">
                 <div className="row">
                     <div className="col d-flex justify-content-evenly">
+
+                        {/* Filter transactions */}
                 <button className='rounded-4 d-flex align-items-center '>
                   
                   <Settings2Icon color='blue' className='me-2 transactions-options-icon'/>  
@@ -24,12 +26,96 @@ function Transactions() {
                 </div>
             </div>
 
-                <div className="row"></div>
+        
+            
+              <div className="row transaction-history-container d-flex justify-content-center">
 
+       {/* Heading */}
+           <div className='col-12 col-xl-8 mb-1 transaction-card-heading d-flex justify-content-between'>
+                    <span className='mb-0 text-center fw-bold'>
+                        {[].length + ' Transaction(s) are found'}
+                    </span>
+    
+                      {/* remaining  budget*/}
+                      <span className='text-center fw-normal'>
+                        Budget left:
+                        <span className=' ms-1 text-center text-danger fw-bold'>
+                             $500
+                            </span>
+                        </span>
+                </div>
+
+         {/* Each transaction */}
+       { []?.length ? ( 
+            [{}].map((tran, index) => (
+    <div className={`col-12 mb-2 col-xl-8 transaction-card py-3`}
+    key={index} id={index}>
+
+                     {/*  transaction details */}
+                     <div className='mb-3 d-flex justify-content-between align-items-center'>
+                        {/* transaction category (food, education, etc)*/}
+                        <span 
+                           className='fw-bold transaction-category mb-0 d-flex align-items-center'>
+                            {tran.category + ' |'}
+        
+                           {/* transaction type (expense or salary)*/}
+                           <span className='fw-normal ms-1 transaction-type'>
+                              {tran.type}
+                            </span>
+                            </span>
+    
+                            { /* options */}
+                            <span  className='mb-0'>
+                           {/* Delete transaction */}
+                           <button className='btn text-danger transaction-card-opt text-white p-1'>
+                                    Delete
+                            </button>                            
+                            </span >     
+                     </div>
+                         
+                         
+
+               {/* transacto=ion amount */}
+                <p 
+                className={`ms-1 fw-bold 
+                ${tran.type ==='Salary' ? 'text-success' : 'text-danger' }`}>
+                    ₹{tran.amount}
+                </p>
+
+                {/* transaction added dated */}
+                <p 
+                className={`ms-1 pb-1 transaction-date border-bottom border-secondary fw-bold`}>
+                      {tran.createdAt}
+                </p>
+
+
+         {/* transaction description */}
+           <span className='ms-1 fw-bold'>Description: </span>
+              <p className='d-inline transaction-description'>
+                    {tran.description || 'No description provided'}
+                    </p>
+
+                    </div>
+    ))
+) : (
+       //  when user has 0 transactions
+        <div className='d-flex mt-2 justify-content-center'>
+            <h4>
+                No transactions found
+            </h4>
+            </div>)}
 
         </div>
+        </div>
 
-        
+
+         {/* Transaction ended message */}
+        <div className='d-flex mb-5 mt-2 justify-content-center'>
+            <h4>
+               { []?.length ?  "That's all you've transacted )": ''}
+                
+            </h4>
+            </div>
       
     </div>
   )
