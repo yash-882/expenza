4478429550277aed753aff368de6bf06827b9935
yash <yashh2nd@gmail.com>
2025-06-15 11:52:22 +0530

    feat: Add component to wrap Popups

---
+++ frontend/src/components/PopupWrapper.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function PopupWrapper(props) {
+  return (
+    <div
+    className='d-flex justify-content-center px-4  flex-column align-items-center' 
+    style={{
+        width: '100%', 
+        height: '100%', 
+        backgroundColor: 'rgba(0, 0, 0, 0.82)',
+        position: 'fixed',
+        zIndex: 10000,
+        top: 0,
+        overflow:'auto'
+    }}>
+        {props.children}
+
+    </div>
+  )
+}
+
+export default PopupWrapper
