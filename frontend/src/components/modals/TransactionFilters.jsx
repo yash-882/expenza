556a093437cc17c6a1e044890cd6994fc17f19ad
yash <yashh2nd@gmail.com>
2025-06-16 21:19:28 +0530

    feat: Add component to create layout to display Transaction filters slide-panel

---
+++ frontend/src/components/modals/TransactionFilters.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import PopupWrapper from '../PopupWrapper'
+import { transactionCategories } from '../../constants/transactions/transaction-categories'
+import {Settings2Icon} from 'lucide-react'
+function TransactionFilters() {
+
+  return (
+    <PopupWrapper>
+        {/*  */}
+               {/* contains categories and amount */}
+        <div className='px-2 py-2 d-flex flex-column align-items-start justify-content-center  transaction-filter-silde-panel bg-light'>
+
+        <div className='border-bottom d-flex py-1 w-100'>
+    {/* heading */}
+                {/* icon */}
+                <Settings2Icon size={30} className=' ms-1 text-primary'/>
+             <h4 className='ms-1 fw-bold'>Find by filters 
+                
+                </h4>
+            
+        </div>
+        
+                     {/* // categories */}
+                 <div className='d-flex mb-2 flex-wrap transactions-categories-list pt-3' aria-multiselectable={true}>
+                     <h5 className='me-2  py-1  fw-bold mb-0'>Categories</h5>
+                {
+                    // category option
+                    transactionCategories.map((categ, index) => (
+
+                        <div className="me-2 mb-2 fw-bold px-2 py-1  transaction-category-option" 
+                        data-id={categ}
+                        key={index}>
+                        
+                            {categ[0].toUpperCase() + categ.slice(1, categ.length)}
+
+                        </div>
+                            
+                    ))
+                }
+
+
+                </div>
+                {/* option to sort by amount */}
+                    <div className='d-flex flex-wrap mb-3 transactions-categories-list'>
+                     <h5 className='me-2 py-1 fw-bold mb-0'>Sort by Amount</h5>
+
+{/* sort by lowest to highest amount */}
+                 <div 
+                 role='button' 
+                 className="me-2 mb-2 fw-bold px-2 py-1 transaction-category-option">
+                    Lowest to highest
+
+                    </div>
+
+{/* sort by highest to lowest amount */}
+
+                 <div 
+                 role='button' 
+                 className="me-2 mb-2 fw-bold  px-2 py-1 transaction-category-option">
+                    Highest to lowest
+                    </div>
+
+                </div>
+
+                <div 
+                className='filter-panel-btn-group '>
+
+                     {/* apply filters */}
+                    <button className='btn mb-1 me-2 text-primary fw-bold'>
+                        Apply
+                    </button>
+                    {/* clear filters */}
+                    <button className='btn mb-1 me-2 text-danger fw-bold'>
+                        Clear Filters
+                    </button>
+                    {/* close panel */}
+                    <button className='btn mb-1 btn-secondary fw-bold'>
+                        Close
+                     </button>
+
+
+                </div>
+
+        </div>
+
+
+    </PopupWrapper>
+      
+    
+  )
+}
+
+export default TransactionFilters
