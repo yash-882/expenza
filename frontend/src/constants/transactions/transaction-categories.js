1b557df82467e2fcae438ee9438b39bbad50f6a2
yash <yashh2nd@gmail.com>
2025-06-18 07:36:23 +0530

    fix: Add property 'emoji' in transaction-categories.js(constant)  to store emojis for creating valid field name before fetching data using query strings

--- frontend/src/constants/transactions/transaction-categories.js
+++ frontend/src/constants/transactions/transaction-categories.js
@@ -1,6 +1,13 @@
-export const transactionCategories = [...new Set(
-      ['food', 'transport', 'housing', 
-    'entertainment', 'healthcare', 'education', 
-    'job', 'freelance', 'bonus', 
-    'other expense', 'other income']
-)]
+export const transactionCategories = [
+  { id: 'food',        emoji: '🍽️' },
+  { id: 'transport',   emoji: '🚌' },
+  { id: 'housing',     emoji: '🏠' },
+  { id: 'entertainment', emoji: '🎮'},
+  { id: 'healthcare',  emoji: '💊' },
+  { id: 'education',   emoji: '🎓' },
+  { id: 'job',         emoji: '💼' },
+  { id: 'freelance',   emoji: '💻' },
+  { id: 'bonus',       emoji: '💰' },
+  { id: 'other expense',  emoji: '' },
+  { id: 'other income',  emoji: '' }
+]
