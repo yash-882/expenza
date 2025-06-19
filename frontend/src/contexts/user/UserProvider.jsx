47f208743d1e41ffd99c94af9f8867233a525a4a
yash <yashh2nd@gmail.com>
2025-06-20 01:53:07 +0530

    feat: Add react context to manage user authentication status

---
+++ frontend/src/contexts/user/UserProvider.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { UserContext } from "../UserContext";
+
+
+function UserProvider(props) {
+    let [isAuthenticated, setIsAuthenticated] = useState(null)
+
+    async function isUserAuthenticated(){
+        try{
+          // throws error if unauthorized to this page
+        await axios.get('http://192.168.1.7:8000/api/transaction', {
+          withCredentials: true
+        })
+    }
+    catch(err){
+            if(err.response){
+              // not authenticated
+            setIsAuthenticated(false)
+            }  
+        }
+    }
+
+    useEffect(()=> {
+      isUserAuthenticated()
+    }, [])
+
+
+  return (
+    <UserContext.Provider   value={{isAuthenticated, setIsAuthenticated}}>
+
+      { props.children }
+
+    </UserContext.Provider>
+  )
+}
+
+export default UserProvider
