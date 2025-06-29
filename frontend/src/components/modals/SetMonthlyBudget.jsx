3a8db58062c7275fd8434cdcddafed7ffa45d9fc
yash <yashh2nd@gmail.com>
2025-06-30 01:12:08 +0530

    feat: Make API call to set monthly budget

--- frontend/src/components/modals/SetMonthlyBudget.jsx
+++ frontend/src/components/modals/SetMonthlyBudget.jsx
@@ -1,11 +1,58 @@
 import React, { useState } from 'react'
 import PopupWrapper from '../PopupWrapper'
 import axios from 'axios'
+import NotificationPopup from './NotificationPopup'
 
 function SetMonthlyBudget({hidePopup}) {
-    let [budget, setBudget] = useState(0);
+    let [budget, setBudget] = useState(0)
+    let [notify, setNotify] = useState(false)
+    let [notificationInfo, setNotificationInfo] = useState({type: '', message: ''})
+    let [loading, setLoading] = useState(false)
+
+    async function handleSettingBudget(evt) {
+        evt?.preventDefault()
+
+        try{
+            // reset notification
+            setNotificationInfo({type: '', message: ''})
+            setLoading(true)
+
+            // setting budget...
+            const response = await axios.patch('http://192.168.1.7:8000/api/user/setting/set-budget',
+                {budget}, 
+                {withCredentials: true}
+            )
+
+            const apiResponse = response.data
+            setLoading(false) //remove loading
+
+            //  state that contains success message
+            setNotificationInfo({type: 'success', message: apiResponse.message})
+
+            // show popup
+            setNotify(true);
+
+           // remove the popup in 2.5 seconds after notifying(success)
+            setTimeout(() => {
+                hidePopup()
+            }, 2500)
+
+        } catch(err){
+            //  state that contains error message
+            // 
+             setNotificationInfo({
+                type: 'error', 
+                message: err.response?.data.message || 'Server error, please try again later'
+            })
+            // show popup
+            setNotify(true);
+        }
+
+        
+    }
 
   return (
+    <>
  <PopupWrapper>
     <div className='bg-light py-4 d-flex flex-wrap flex-column  px-4 rounded-4'
     style={{maxWidth: '400px'}}>
@@ -15,12 +62,14 @@ function SetMonthlyBudget({hidePopup}) {
         </h5>
 
         {/* set budget form */}
-        <form className='w-100'>
+        <form className='w-100'
+        onSubmit={handleSettingBudget}>
                 <input
                 className='p-2 border-0 rounded-3 w-100 mb-4'
                 placeholder='₹ Enter budget amount'
                 style={{outline: "solid 3px rgb(157, 181, 218)"}}
-                type="text"
+                type="number"
+
                 inputMode='numeric'
                 onChange={(evt) => setBudget(evt.target.value)}/>
 
@@ -28,8 +77,17 @@ function SetMonthlyBudget({hidePopup}) {
             <div className='btn-block mb-3 w-100 d-flex flex-wrap flex-column justify-content-between'>
                 {/* submit budget amount */}
                 <button 
-                className="btn btn-primary fw-bold mb-2">
-                    Submit
+                className="btn btn-primary fw-bold mb-2 d-flex justify-content-center align-items-center">
+                    {/* loader */}
+                    {
+                        loading ? 
+
+                        <p className='loader text-center mb-0'
+                        style={{width: '24px', height: '24px'}}>
+                        </p> :
+
+                        'Submit'
+                    }
                 </button>
 
                 {/* close popup */}
@@ -59,6 +117,11 @@ function SetMonthlyBudget({hidePopup}) {
 
 
     </PopupWrapper>
+        {/* show messages regarding budget updation */}
+   { notify && <NotificationPopup
+    removePopup={() => setNotify(false)}
+    notificationInfo={notificationInfo}/>}
+    </>
   )
 }
 
