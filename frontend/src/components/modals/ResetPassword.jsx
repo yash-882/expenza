a29ea56ce27db13793631f5acd487b6a1df1b296
yash <yashh2nd@gmail.com>
2025-07-03 17:07:27 +0530

    feat: Make API call to request OTP for resetting the password

--- frontend/src/components/modals/ResetPassword.jsx
+++ frontend/src/components/modals/ResetPassword.jsx
@@ -1,19 +1,77 @@
 import React, { useState } from 'react'
 import PopupWrapper from '../PopupWrapper';
+import axios from 'axios';
+import OtpPopup from './OtpPopup';
+import NotificationPopup from './NotificationPopup';
 
 // send OTP to email to reset the password
 function ResetPassword({closePopup}) {
     let [loading, setLoading] = useState(false);
+    let [email, setEmail] = useState('')
+    let [hidden, setHidden] = useState(false)
+    let [isOtpSent, setOtpSent] = useState(false)
+         // state for displaying popups
+      let [notificationPopup, showNotificationPopup] = useState(false)
+            
+              // state for error or success messages
+      let [notificationInfo, setNotificationInfo] = useState({
+              type: '', 
+              message: '',
+          }); 
+
+    async function requestOTP(evt){
+
+        evt.preventDefault()
+        setLoading(true) //show loading
+            try{
+                // send OTP to email for resetting the password...
+                const response = await axios.post('http://192.168.1.7:8000/api/auth/reset-password-otp',
+                {email}, 
+                {withCredentials: true})
+
+                setLoading(false) //remove loader
+                // api response
+            const apiResponse = response.data;
+            
+            //  state that contains success message
+            setNotificationInfo({type: 'success', message: apiResponse.message})
+
+            // show notification popup
+            showNotificationPopup(true)
+
+            // hide current ResetPassword popup and show the OTP popup
+            setHidden(true)
+
+            // otp sent
+            setOtpSent(true)
+
+        }
+        catch(err){
+             setLoading(false) //remove loading   
+
+            //  state that contains error message
+            setNotificationInfo({
+                type: 'error', 
+                message: err.response?.data.message || 'Server error, please try again later'
+            })
+
+        // show popup
+        showNotificationPopup(true)
+        }
+
+    }
 
 
   return (<>
-    <PopupWrapper >
+
+    {/* hide popup when another popup displays(OTP popup) */}
+    <PopupWrapper hidden={hidden} >
 
         {/* profile updation popup (PATCH) */}
     <div 
     className={`p-4 d-flex flex-column bg-light  justify-content-center rounded-4`}>
 
-        <form>
+        <form onSubmit={requestOTP}>
 
 {/* label for changing email */}
             <label htmlFor="modify-profile" className='mb-4'>
@@ -28,6 +86,8 @@ function ResetPassword({closePopup}) {
                {/* input to enter email */}
                 <input 
                 type="text"
+                value={email}
+                onChange={(evt)=> setEmail(evt.target.value)}
 
                 id='modify-profile'
                 className=' rounded-3 p-2 w-100 border-0'
@@ -45,6 +105,7 @@ function ResetPassword({closePopup}) {
             type='submit'
             className='text-white d-flex justify-content-center align-items-center btn fw-bold bg-primary mb-2 rounded-3 border-0'
             disabled={loading}>
+
                 {loading ? 
                 // show loading...
                 <p 
@@ -71,6 +132,30 @@ function ResetPassword({closePopup}) {
     </div>
 
     </PopupWrapper>
+
+    {/* error or success message after submiting OTP */}
+    {
+        notificationPopup && 
+        <NotificationPopup
+        notificationInfo={notificationInfo}
+        removePopup={()=> showNotificationPopup(false)}/>
+    }
+
+    {
+        // show OTP form popup after successful delivery
+        isOtpSent && 
+        <OtpPopup
+        heading='Enter OTP to reset password'
+        sentTo={email}
+        setNotificationInfo={setNotificationInfo}
+        notificationInfo={notificationInfo}
+        removeNotificationPopup={()=> showNotificationPopup(false)}
+        setNotify={showNotificationPopup}
+        notify={notificationPopup}
+        resendOtp={requestOTP}
+        />
+    
+    }
 </>
   )
 }
